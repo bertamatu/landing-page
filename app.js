f23e6dc64396da0a8de254ce8a50b48523b28508
Berta Matu <info.berta@gmail.com>
2020-05-31 20:12:32 +0300

    basic v1.0

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-function navbarToggle() {
-  const navbar = document.getElementById("myTopnav");
-  if (navbar.className === "topnav") {
-    navbar.className += " responsive";
-  } else {
-    navbar.className = "topnav";
-  }
-}
-
 function playHello() {
   const helloSound = document.getElementById("hello_sound");
   helloSound.play();
