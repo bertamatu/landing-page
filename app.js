28a6721ba18f9a0506f2c3f7ad9ade50e0830987
bertamatu <info.berta@gmail.com>
2020-06-03 16:13:17 +0300

    work appear js

--- app.js
+++ app.js
@@ -7,9 +7,20 @@ function playHello() {
 function aboutTextAppear() {
   const aboutText = document.querySelector(".about_text");
   const aboutTextPosition = aboutText.getBoundingClientRect().top;
-  const screenPosition = window.innerHeight / 2;
+  const screenPosition = window.innerHeight / 1.6;
   if (aboutTextPosition < screenPosition) {
     aboutText.classList.add("about_text_appear");
   }
 }
 window.addEventListener("scroll", aboutTextAppear);
+
+//work text appear on scroll
+function workTextAppear() {
+  const workText = document.querySelector(".work_text");
+  const workTextPosition = workText.getBoundingClientRect().top;
+  const screenPosition = window.innerHeight / 1.7;
+  if (workTextPosition < screenPosition) {
+    workText.classList.add("work_text_appear");
+  }
+}
+window.addEventListener("scroll", workTextAppear);
