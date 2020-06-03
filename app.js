5de71756611a41fca58a13e567a3fdb5e250ffb0
bertamatu <info.berta@gmail.com>
2020-06-03 15:20:41 +0300

    about text appear app.js

--- app.js
+++ app.js
@@ -2,3 +2,14 @@ function playHello() {
   const helloSound = document.getElementById("hello_sound");
   helloSound.play();
 }
+
+//about text appear on scroll
+function aboutTextAppear() {
+  const aboutText = document.querySelector(".about_text");
+  const aboutTextPosition = aboutText.getBoundingClientRect().top;
+  const screenPosition = window.innerHeight / 2;
+  if (aboutTextPosition < screenPosition) {
+    aboutText.classList.add("about_text_appear");
+  }
+}
+window.addEventListener("scroll", aboutTextAppear);
